fd7a55147065608380aecbd4d60329d168d25d3b
Yann PETITJEAN <yann120@hotmail.fr>
2019-08-24 13:11:24 +0000

    add fullname to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fullname, presence: true, length: {minimum: 3, maximum: 50}
 end
