174c0ef1ae043c85e455b3d2694a97a95fe362d3
Yann PETITJEAN <yann120@hotmail.fr>
2019-08-26 16:45:37 +0000

    add room controller

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,31 @@
+class RoomController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def listing
+  end
+
+  def pricing
+  end
+
+  def description
+  end
+
+  def photo_upload
+  end
+
+  def amenities
+  end
+
+  def location
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'room/index'
+  get 'room/new'
+  get 'room/create'
+  get 'room/update'
+  get 'room/listing'
+  get 'room/pricing'
+  get 'room/description'
+  get 'room/photo_upload'
+  get 'room/amenities'
+  get 'room/location'
   root 'pages#home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
   resources :users, only: [:show]
