ed05f74b3d6b5e0e97ec5970dde4f0212960e552
Yann PETITJEAN <yann120@hotmail.fr>
2019-09-03 08:47:53 +0000

    add publish verifications on room menu

--- app/helpers/room_helper.rb
+++ app/helpers/room_helper.rb
@@ -1,2 +1,13 @@
 module RoomHelper
+    def is_done(fields = nil)
+        return "<span class='icon is-right has-text-success'><i class='fa fa-check'></i></span>".html_safe if !fields
+        fields.each do |field|
+            return if field.blank?
+        end
+        "<span class='icon is-right has-text-success'><i class='fa fa-check'></i></span>".html_safe
+    end
+
+    def is_publishable(room)
+        return !room.price.blank? && !room.listing_name.blank? && !room.summary.blank? && !room.room_pictures.blank? && !room.address.blank? 
+    end
 end
