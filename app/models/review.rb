9524007565a8ca71889f35fe2ea472d9e979f234
Yann PETITJEAN <yann120@hotmail.fr>
2019-09-20 17:53:12 +0000

    add from_user review method and average rating in show user

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,12 @@ class Review < ApplicationRecord
             "guest_review"
         end
     end
+
+    def from_user
+       if self.reservation.room.user_id == self.user_id
+            self.reservation.user
+        else
+            self.reservation.room.user
+        end 
+    end
 end
